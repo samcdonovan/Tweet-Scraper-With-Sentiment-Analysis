9d8d186287c2bfd2925aacb3ded5c5d39fed84b9
samcdonovan <samcdonovan@gmail.com>
2021-12-15 20:30:27 +0000

    Initial message

---
+++ README.md
@@ -0,0 +1,5 @@
+# TweetScraper-samcdonovan Package
+
+Twitter Scraper that runs sentiment analysis on scraped tweets to gauge public opinion about different companies' climate change performance.
+
+## What is scraping?
---
+++ src/main.py
@@ -0,0 +1,35 @@
+import requests
+import mysql.connector
+import tweepy
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler("WESM4XEo8vZYv5AARe4gdNWTA", 
+    "VwxQHFdpFr5elAcEYVIiAF7daX6opH04HemyLSyeC2aUehlf92")
+auth.set_access_token("1455271107897004037-trpcifKKBpUrlEbOvFPqt4sw2CSXq6", 
+"kHYrzrpwtLso2cAqBuh9I1xiXhLQFz7oNAVARunnKNp8N")
+
+api = tweepy.API(auth)
+
+#Create API object
+#api = tweepy.API(auth, wait_on_rate_limit=True,
+  #  wait_on_rate_limit_notify=True)
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
+
+
+for tweet in api.search_tweets(q="#amazon #climatechange", lang="en", rpp=10):
+    print(f"{tweet.user.name}:{tweet.text}")
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password=""
+)
+
+print(mydb)
+response = requests.get('https://httpbin.org/ip')
+
+print('Your IP is {0}'.format(response.json()['origin']))
