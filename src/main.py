086308f01ed8dd31453aee1ea5d6a54d21446a27
samcdonovan <samcdonovan@gmail.com>
2021-12-16 12:55:44 +0000

    Fixed DB connection

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import requests
+import tweetDAO as dao
 
 response = requests.get('https://httpbin.org/ip')
 
 print('Your IP is {0}'.format(response.json()['origin']))
+
+dao.initDB()
--- src/tweetDAO.py
+++ src/tweetDAO.py
@@ -4,11 +4,11 @@ from sqlalchemy import create_engine
 
 import mysql.connector
 
-
-#engine = sal.create_engine("mysql://user:root@localhost/scraped_tweets?charset=utf8mb4")
-engine = create_engine('mysql+pymysql://root:password@127.0.0.1/scraped_tweets?') 
-#engine = sal.create_engine(‘mssql+pyodbc://local_host/scraped_tweets?driver=SQL Server?Trusted_Connection=yes’)
-print(engine.table_names())
+def initDB():
+    #engine = sal.create_engine("mysql://user:root@localhost/scraped_tweets?charset=utf8mb4")
+    engine = create_engine('mysql+pymysql://root:''@127.0.0.1/scraped_tweets') 
+    #engine = sal.create_engine(‘mssql+pyodbc://local_host/scraped_tweets?driver=SQL Server?Trusted_Connection=yes’)
+    print(engine.table_names())
 
 #mydb = mysql.connector.connect(
  # host="localhost",
