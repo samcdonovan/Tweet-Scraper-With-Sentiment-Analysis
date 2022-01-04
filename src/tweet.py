7bc98d4a74c43e1fff99fe7532f249a9467bc64b
samcdonovan <samcdonovan@gmail.com>
2022-01-04 16:57:16 +0000

    Added convert_to_list, updated tweet.py

--- src/tweet.py
+++ src/tweet.py
@@ -1,7 +1,8 @@
 
 class Tweet:
     
-    def __init__(self, id, text, time):
+    def __init__(self, id, original_tweet, processed_text, time):
         self.id = id
-        self.text = text
+        self.original_tweet = original_tweet
+        self.processed_text = processed_text
         self.time = time
