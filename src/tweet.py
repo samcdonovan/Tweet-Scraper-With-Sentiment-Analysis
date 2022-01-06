79311034bf1eb188304387d6f023d2a70fdd0f07
samcdonovan <samcdonovan@gmail.com>
2022-01-06 15:04:18 +0000

    processed_text -> cleaned_text

--- src/tweet.py
+++ src/tweet.py
@@ -1,8 +1,8 @@
 
 class Tweet:
     
-    def __init__(self, id, original_tweet, processed_text, time):
+    def __init__(self, id, original_tweet, cleaned_text, time):
         self.id = id
         self.original_tweet = original_tweet
-        self.processed_text = processed_text
+        self.cleaned_text = cleaned_text
         self.time = time
