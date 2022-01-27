59057d289d53a49782b7687ca601218e3ddae0f2
Sam.Donovan <samcdonovan@gmail.com>
2022-01-27 13:54:09 +0000

    Added company field, removed id field

--- src/tweet.py
+++ src/tweet.py
@@ -1,8 +1,7 @@
 
 class Tweet:
     
-    def __init__(self, id, company, original_tweet, cleaned_text, time):
-        self.id = id
+    def __init__(self, company, original_tweet, cleaned_text, time):
         self.company = company
         self.original_tweet = original_tweet
         self.cleaned_text = cleaned_text
